87c2b2756f902fabee4f4b2cbac1b0b888a8019f
Ives <ives.sato@gmail.com>
2026-04-16 18:08:56 -0300

    Minha atualizacao adicionando  B e C no program.js

--- saip/program.js
+++ saip/program.js
@@ -2,6 +2,20 @@ class A {
   static max = 1000;
   
   constructor() {
-    this.children = ["A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8"];
+    this.children = ["A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10"];
+  }
+}
+class B {
+  static max = 1000;
+  
+  constructor() {
+    this.children = ["B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"];
+  }
+}
+class C {
+  static max = 1000;
+  
+  constructor() {
+    this.children = ["C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"];
   }
 }
